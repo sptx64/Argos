867173dd3448c2d60c41e7f1d62455496a7d432e
sptx64 <89103497+sptx64@users.noreply.github.com>
2023-12-30 21:17:26 +1100

    Update logging.py

--- app/logging.py
+++ app/logging.py
@@ -15,6 +15,8 @@ def logging(secret1, secret2) :
         password2 = col2.text_input("Insert access password2", type="password")
         st.write(password1)
         st.write(secret1)
+        st.write(password1 != secret1)
+        st.write(password2 != secret2)
         connect = col2.button("Connect", type="primary", disabled = False if st.session_state.wait==False else True)
         if connect :
             if (password1 != secret1) | (password2 != secret2) :
