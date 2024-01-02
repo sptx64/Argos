dab5b0f84f8d85bf9841f55cd355add1651768a3
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-01-02 22:31:16 +1100

    Update Panoptes.py

--- Panoptes.py
+++ Panoptes.py
@@ -3,18 +3,18 @@ import os
 
 st.set_page_config(layout = 'wide')
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    } header { visibility: hidden; }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# st.markdown(
+#     """
+#     <style>
+#     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+#     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+#     .viewerBadge_text__1JaDK {
+#         display: none;
+#     } header { visibility: hidden; }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 
 from app.logging import logging
