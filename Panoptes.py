54a7a09f97bd3e310ef5a8c80294485dbb7ef707
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-01-02 22:26:05 +1100

    Update Panoptes.py

--- Panoptes.py
+++ Panoptes.py
@@ -3,6 +3,20 @@ import os
 
 st.set_page_config(layout = 'wide')
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
 from app.logging import logging
 logging(st.secrets["secret1"], st.secrets["secret2"])
 
