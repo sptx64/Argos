5b57a59a6e95f899f6f75859bd842a363123fc19
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-02-03 22:50:42 +1100

    Update logging.py

--- app/logging.py
+++ app/logging.py
@@ -11,19 +11,20 @@ def logging(secret1, secret2) :
 
     if "password" not in st.session_state :
         col1,col2,col3 = st.columns(3)
-        password1 = col2.text_input("Insert access password1", type="password")
-        password2 = col2.text_input("Insert access password2", type="password")
-        connect = col2.button("Connect", type="primary", disabled = False if st.session_state.wait==False else True)
-        if connect :
-            if (password1 != secret1) | (password2 != secret2) :
-                st.session_state.tries+=1
-                col2.error(f"Try number : {st.session_state.tries}")
-                if st.session_state.tries > 3 :
-                    st.session_state.wait=True
+        with col2.form("logging"):
+            password1 = col2.text_input("Insert access password1", type="password")
+            password2 = col2.text_input("Insert access password2", type="password")
+            connect = col2.form_submit_button("Connect", type="primary", disabled = False if st.session_state.wait==False else True)
+            if connect :
+                if (password1 != secret1) | (password2 != secret2) :
+                    st.session_state.tries+=1
+                    col2.error(f"Try number : {st.session_state.tries}")
+                    if st.session_state.tries > 3 :
+                        st.session_state.wait=True
+                        st.rerun()
+                else :
+                    st.session_state.password=True
                     st.rerun()
-            else :
-                st.session_state.password=True
-                st.rerun()
         st.stop()
 
 # logging(secret1,secret2)
