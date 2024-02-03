d156ecc99e26196a4dda710b8144cbb4072a9b8b
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-02-03 23:00:40 +1100

    Update logging.py

--- app/logging.py
+++ app/logging.py
@@ -12,9 +12,9 @@ def logging(secret1, secret2) :
     if "password" not in st.session_state :
         col1,col2,col3 = st.columns(3)
         with col2.form("logging"):
-            password1 = col2.text_input("Insert access password1", type="password")
-            password2 = col2.text_input("Insert access password2", type="password")
-            connect = col2.form_submit_button("Connect", type="primary", disabled = False if st.session_state.wait==False else True)
+            password1 = st.text_input("Insert access password1", type="password")
+            password2 = st.text_input("Insert access password2", type="password")
+            connect = st.form_submit_button("Connect", type="primary", disabled = False if st.session_state.wait==False else True)
             if connect :
                 if (password1 != secret1) | (password2 != secret2) :
                     st.session_state.tries+=1
