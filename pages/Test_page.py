86edc72c17426e1f65080fe4e48ad7af4da5c593
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-03-29 18:14:13 +1100

    Update Test_page.py

--- pages/Test_page.py
+++ pages/Test_page.py
@@ -491,7 +491,10 @@ if len(RSIs) > 0 :
     subplot_row+=1
 
 if bind :
-  data
+    data["bind"] = ((data["ao"]/data["ao"].max()) + (data["Mom"]/data["Mom"].max()))/2
+    data_bull_bind = data[data["bind"]>data["bind"].shift(1)]
+    data_bear_bind = data[data["bind"]<data["bind"].shift(1)]
+    fig.add_trace(go.Scatter(x=data_bull_bind["Date"], y=data_bull_bind["Low"], mode='markers', marker_color='limegreen', marker_symbol="triangle-up", showlegend=False), col=1, row=1)
 
 if AO :
     for df, color in zip([neg_rising, pos_rising, pos_falling, neg_falling],["lightseagreen", "lightseagreen", "red", "red"]) :
