0781e4fd2cf0d9c02d6c10cfe9b3ef586a703057
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-07-22 09:36:04 +1100

    Update gg_drive.py

--- app/gg_drive.py
+++ app/gg_drive.py
@@ -9,9 +9,9 @@ def upload_file() :
   media = MediaFileUpload(file_path, mimetype='text/plain')
   file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
-def download_file() :
-  file_id = 'file_id_to_download'
-  file_path = 'path/to/save/downloaded/file.txt'
+def download_file(file_id, file_path) :
+  # file_id = 'file_id_to_download'
+  # file_path = 'path/to/save/downloaded/file.txt'
 
   request = drive_service.files().get_media(fileId=file_id)
   fh = io.FileIO(file_path, mode='wb')
