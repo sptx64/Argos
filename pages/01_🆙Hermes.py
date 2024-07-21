403e2eac9deecdbbcb90570c827e243565b0fbfd
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-07-22 09:11:43 +1100

    Update 01_🆙Hermes.py

--- pages/01_🆙Hermes.py
+++ pages/01_🆙Hermes.py
@@ -71,6 +71,7 @@ dict_up = {}
 for dbases_path in [os.path.join("dataset",x) for x in ["sp500", "crypto_binance", "crypto_coinbase"]] :
     dict_up[dbases_path] = {}
     for f in [x for x in os.listdir(dbases_path) if x.endswith(".parquet")] :
+        f
         dict_up[dbases_path][f] = os.getmtime(os.path.join(dbases_path, f))
     if len(dict_up[dbases_path]) == 0 :
         continue
