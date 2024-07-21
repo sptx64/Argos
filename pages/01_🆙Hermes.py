5f626ec942c87fe87459803668e7e5e02215fc22
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-07-22 08:54:02 +1100

    Update 01_🆙Hermes.py

--- pages/01_🆙Hermes.py
+++ pages/01_🆙Hermes.py
@@ -256,8 +256,10 @@ with st.sidebar.popover("Dev update tool", use_container_width=True) :
         return buf
 
 
-    if st.button("Compute zipped parquet database") :
-        st.download_button(label="Download .zip", data=zip_and_download().getvalue(), file_name="historical_data.zip", mime="application/zip", help="upload the zipfile to g drive")
+    if st.button("Compute zipped parquet database", use_container_width=True) :
+        st.download_button(label="Download .zip", data=zip_and_download().getvalue(),
+                           file_name="historical_data.zip", mime="application/zip",
+                           use_container_width=True, type="primary", help="upload the zipfile to g drive")
 
     # if st.toggle('Download zipped .parquets', help = 'download zip of all the .parquets available in the cloud. Useful to upload it on a gdrive') :
     #     list_paths = [os.path.join("dataset", x) for x in ["sp500","crypto_coinbase", "crypto_binance"] ]
