f0dc11dcd8ba814c01552add787bda55d965560b
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-07-22 09:52:29 +1100

    Update 01_🆙Hermes.py

--- pages/01_🆙Hermes.py
+++ pages/01_🆙Hermes.py
@@ -133,7 +133,6 @@ if update :
                 if len(data) > 10 :
                     to_drop=9
                     from_date = data["Date"].astype(str).values[-to_drop][:10]
-                    from_date
                     new_data = yf.download(tickers = tick, start = from_date, interval = "1d").reset_index()
                     data.drop(data.tail(to_drop).index, inplace=True)
                     data=pd.concat([data,new_data], ignore_index=True)
