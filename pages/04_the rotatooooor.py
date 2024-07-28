53a0c62c33a562f592e0ed59d8f47082e691805b
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-07-28 22:40:42 +1100

    Update 04_the rotatooooor.py

--- pages/04_the rotatooooor.py
+++ pages/04_the rotatooooor.py
@@ -54,7 +54,6 @@ for t in ticker :
     file_path = os.path.join(path, f"{t}.parquet")
     file_path
     dfs[t] = pd.read_parquet(file_path)
-    dfs[t]
     
     for elem in ["High","Low","Open","Close","Volume"] :
         dfs[t][elem] = dfs[t][elem].astype(float)
