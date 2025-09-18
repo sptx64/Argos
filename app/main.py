d1a7b8afd4366a8ce2c284c7414b9c900425224b
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-18 16:53:24 +1100

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+# if st.context.theme["type"] == "light" :
+#     st.logo("img/Zero dark writing.png", icon_image="img/Zero dark.png", size="large")
+# else :
+#     st.logo("img/Zero white.png", icon_image="img/Zero white.png", size="large")
+
+pages_dict = {"HOME":[st.Page("directory/00-Panoptes.py", title="Home", icon=":material/home:", default=True)],
+                "UPDATE":[ st.Page("directory/01_Hermes.py", title="Hermes", icon=":material/cloud:"),],
+                "EXPLORE":[st.Page("directory/02_️Argos.py", title="Argos", icon=":material/ophthalmology:"),
+                            st.Page("directory/03_Zeus.py", title="Zeus", icon=":material/bolt:"),] }
+                            # st.Page("directory/03_Zeus_echarts.py", title="Zeus_ec", icon=":material/bolt:")] }
+
+pg = st.navigation(pages_dict, position="top")
+pg.run()
+
+
+from functions.logging import logging
+logging(st.secrets["secret1"], st.secrets["secret2"])
