cb783ea30ebc26d11f8d2785d3a9ef365a024b8c
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-18 16:55:57 +1100

    Add files via upload

---
+++ app/functions/logging.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import os
+
+
+def logging(secret1, secret2) :
+    if "wait" not in st.session_state :
+        st.session_state.wait=False
+
+    if "tries" not in st.session_state :
+        st.session_state.tries=0
+
+    if "password" not in st.session_state :
+        col1,col2,col3 = st.columns(3)
+        with col2.form("logging"):
+            password1 = st.text_input("Insert access password1", secret1, type="password").replace("/","").replace(":","").replace("'","")
+            password2 = st.text_input("Insert access password2", secret2, type="password").replace("/","").replace(":","").replace("'","")
+            connect = st.form_submit_button("Connect", type="primary", disabled = False if st.session_state.wait==False else True)
+            if connect :
+                if (password1 != secret1) | (password2 != secret2) :
+                    st.session_state.tries+=1
+                    col2.error(f"Try number : {st.session_state.tries}")
+                    if st.session_state.tries > 3 :
+                        st.session_state.wait=True
+                        st.rerun()
+                else :
+                    st.session_state.password=True
+                    st.rerun()
+        st.stop()
+
+# logging(secret1,secret2)
---
+++ app/functions/pathfunc.py
@@ -0,0 +1,10 @@
+import os
+
+def get_path_data() :
+    return "../data"
+
+def get_path_crypto() :
+    return os.path.join( get_path_data(), "crypto_binance")
+
+def get_path_sp500() :
+    return os.path.join( get_path_data(), "sp500")
