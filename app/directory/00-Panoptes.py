4ae1d09e968ae3f48f8c5eef5da5913b61ef761c
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-19 07:22:36 +1100

    Update 00-Panoptes.py

--- app/directory/00-Panoptes.py
+++ app/directory/00-Panoptes.py
@@ -17,8 +17,8 @@ img_path = os.path.join(pf.get_path_app(), "img/panoptes.jpg")
 st.write(os.listdir("."))
 
 st.write(os.listdir("app"))
-st.write(os.listdir("app/img"))
-
+# st.write(os.listdir("app/img/panoptes.JPG"))
+st.image("app/img/panoptes.JPG")
 
 st.image(img_path, width="stretch")
 st.caption('For educational only, not financial advise')
